e37170d8149fac22f89752fae2d252490bf2b5a6
ErebosXYZ <robertmaynou@gmail.com>
2025-06-18 18:48:19 +0200

    until it 3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -12,7 +15,12 @@ app.use(express.json());
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 
+const MONGODB_URI = "mongodb+srv://RobertMF:zVz8C3as1iXclvIV@cluster0.jrj3qzj.mongodb.net/recipes-2025";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -24,7 +32,34 @@ app.get('/', (req, res) => {
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
+app.post('/recipes', async (req, res) => {
+    const { title, instructions, level, ingredients, image, duration, isArchived, created } = req.body;
 
+    const newRecipe = Recipe({
+        title,
+        instructions,
+        level,
+        ingredients,
+        image,
+        duration, 
+        isArchived,
+        created
+    })
+
+    await newRecipe.save();
+
+    res.status(201).json(newRecipe);
+    
+})
+
+/** title: { type: String, required: true, unique: true },
+    instructions: { type: String, required: true },
+    level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
+    ingredients: { type: [String] },
+    image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
+    duration: { type: Number, min: 0 },
+    isArchived: { type: Boolean, default: false },
+    created: { type: Date, default: Date.now } */
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
